29d9fe4207d55ca59569708b78d36cc77f35fa70
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-09-01 13:11:05 +0900

    [fix] ログアウトを追加

--- vuex-demo/src/views/Profile.vue
+++ vuex-demo/src/views/Profile.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>This is the profile page</h1>
     <p>name: {{name}}</p>
+    <button v-on:click="logout()">logout</button>
   </div>
 </template>
 
@@ -10,6 +11,12 @@ export default {
   computed: {
     name() {
       return this.$store.state.name;
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.commit("logout");
+      this.$router.push("/");
     }
   }
 }
